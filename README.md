34baf2fa240c28cc98fe2aac25ab62370e34f60b
Robert DeForrest Reynolds <rldr.europa@gmail.com>
2025-09-28 04:34:15 -0700

    Enhance comlink.py with comment creation and logging
    Added logging functionality and a comment creation mechanism to comlink.py. The script now reads commands from stdin, supports creating new comments, and logs key actions to stderr. Updated README with a section explaining the motivation behind comlink.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # comlink
 A utility for communicating with text editors a project's external comments
+
+### What is the point?
+
+Because I think documentation, and commenting is important. The thing is, it's incredibly subjective. All comments being there benefits everyone, but everyone personally only needs few comments here and there on things they specifically don't understand.
+As well, for incredibly large, and convoluted projects, it could prove very fruitful to have a heavily-detailed documentation embedded within the source files, but bloating the files themselves has always been a problem.
+
+I made comlink to try to solve that problem, and well, just because I wanted to. I find this tool very useful myself, and that's enough to make it.
--- comlink.py
+++ comlink.py
@@ -1,11 +1,37 @@
-from sys import argv, stdout, stderr
+from sys import argv, stdin, stdout, stderr
 
-stdout.write("running comlink.py")
-stdout.flush()
+
+def log(msg): print(f'{msg}\n', file=stderr, flush=True)
+
+
+workspace_path = argv[1]
+tracker = 0
 
 test_comments = {
-    12348:"I'm a comment"
 }
 
-for arg in argv:
-    print(arg)
+
+log("comlink.py is running...")
+log(f'Workspace: {workspace_path}')
+
+
+def create_comment(comment) -> None:
+    global tracker
+    log(f"Creating comment: {comment}")
+    comment_id = tracker
+    stdout.write(f'~{comment_id}')
+    stdout.flush()
+    test_comments.update({str(tracker):comment})
+    tracker += 1
+
+
+while True:
+    line = stdin.readline().strip()
+    if not line:
+        continue
+    if line[0] == '~':
+        create_comment(line[1:])
+    if line in test_comments:
+        stdout.write(f'{test_comments[line]}\n')
+        stdout.flush()
+        log(f'Received ID: {line}')
